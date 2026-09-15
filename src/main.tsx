ff03e83b9822b4997c6a708b1faea2743dcdd916
thomazdasilvabruno948-create <thomazdasilvabruno948@gmail.com>
2026-09-15 10:54:47 -0300

    feat(seo): initialize metadata for client-side routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { updateSeo } from "./seo";
 import "./styles.css";
 import "./contact-layout.css";
 import "./fields-layout.css";
 import "./detail-layout.css";
+
+updateSeo(window.location.pathname);
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+window.addEventListener("popstate", () => updateSeo(window.location.pathname));
